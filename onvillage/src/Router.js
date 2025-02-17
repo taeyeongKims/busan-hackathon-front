d75f1a38aa500e3365fa8109bcc46e4279114551
Keun04 <128891914+Keun04@users.noreply.github.com>
2025-02-18 02:31:18 +0900

    Feat: 위치 찾기 버튼 생성

--- onvillage/src/Router.js
+++ onvillage/src/Router.js
@@ -3,12 +3,14 @@ import MissionPage from './pages/MissionPage.js';
 import LoginPage from './pages/LoginPage.js';
 import MisstionDetailPage from './pages/MissionDetailPage.jsx';
 import MissionWritePage from './pages/MissionWritePage.jsx';
+import SignUpPage from './pages/SignUpPage.js';
 
 function Router() {
   return (
     <Routes>
       <Route path="/" element={<MissionPage />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignUpPage />} />
       <Route path="/mission-detail" element={<MisstionDetailPage />} />
       <Route path="/mission-write" element={<MissionWritePage />} />
     </Routes>
--- onvillage/src/pages/SignUpPage.js
+++ onvillage/src/pages/SignUpPage.js
@@ -3,6 +3,7 @@ import styles from './SignUpPage.module.css';
 import { useUser } from '../context/UserContext';
 import Layout from '../components/layout/Layout';
 import Logo from '../assets/Logo.png';
+import { FaMapMarkerAlt } from 'react-icons/fa';
 
 export default function SignUpPage() {
   const [formData, setFormData] = useState({
@@ -26,9 +27,11 @@ export default function SignUpPage() {
     <Layout showNavBar={false} showHeader={false}>
       <div className={styles.signupContainer}>
         <div className={styles.signupCard}>
-          <img src={Logo} />
+          <div className={styles.logoBox}>
+            <img src={Logo} />
+          </div>
           <form onSubmit={handleSignup} className={styles.signupForm}>
-            <div>
+            <div className={styles.signupFormItem}>
               <label htmlFor="loginId" className={styles.signupLabel}>
                 아이디
               </label>
@@ -43,7 +46,7 @@ export default function SignUpPage() {
               />
             </div>
 
-            <div>
+            <div className={styles.signupFormItem}>
               <label htmlFor="password" className={styles.signupLabel}>
                 비밀번호
               </label>
@@ -58,7 +61,7 @@ export default function SignUpPage() {
               />
             </div>
 
-            <div>
+            <div className={styles.signupFormItem}>
               <label htmlFor="name" className={styles.signupLabel}>
                 닉네임
               </label>
@@ -71,6 +74,11 @@ export default function SignUpPage() {
                 required
                 className={styles.input}
               />
+            </div>
+
+            <div className={styles.signupLocaBtn}>
+              <FaMapMarkerAlt />
+              <p>나의 위치 찾기</p>
             </div>
 
             <button type="submit" className={styles.signupButton}>
