097eda04c04b7afa1ab8a651805aec1c54f7d4a0
Keun04 <128891914+Keun04@users.noreply.github.com>
2025-02-17 22:20:21 +0900

    Feat: 미션 리스트 구현

---
+++ onvillage/src/components/MissionCard.js
@@ -0,0 +1,35 @@
+import { BiSolidLike } from 'react-icons/bi';
+import { BiSolidCommentDetail } from 'react-icons/bi';
+
+import style from './MissionCard.module.css';
+import Image from '../assets/Cafe.png';
+
+function MissionCard({ nickname, title, image, scrapNumber, applySuccessNumber, date }) {
+  return (
+    <div className={style.cardContainer}>
+      <div className={style.cardTextDiv}>
+        <div className={style.missionTitle}>{title}</div>
+        <div className={style.missionInfo}>
+          <div className={style.missionState}>
+            <div className={style.stateImgBox}>
+              <BiSolidLike className={style.stateImg} />
+              {scrapNumber}
+            </div>
+            <div className={style.stateImgBox}>
+              <BiSolidCommentDetail className={style.stateImg} />
+              {applySuccessNumber}
+            </div>
+          </div>
+          <div>
+            <span>
+              {date} | {nickname}
+            </span>
+          </div>
+        </div>
+      </div>
+      <img src={image} />
+    </div>
+  );
+}
+
+export default MissionCard;
--- onvillage/src/pages/MissionPage.js
+++ onvillage/src/pages/MissionPage.js
@@ -1,13 +1,53 @@
-import LogoImg from '../assets/Logo.png';
+import MissionCard from '../components/MissionCard';
 import style from './MissionPage.module.css';
+
+const dummyData = [
+  {
+    missionId: 1,
+    userId: 'sdfjskjk',
+    nickname: '닉네임입니당',
+    title: '해운대에서 예쁜 바다사진 찍기기',
+    image:
+      'https://search.pstatic.net/sunny/?src=https%3A%2F%2Fimg.freepik.com%2Fpremium-photo%2Fbeautiful-beach-tropical-sea_87394-23002.jpg&type=sc960_832',
+    scrapNumber: 100,
+    applyNumber: 12,
+    applySuccessNumber: 10,
+    date: '2025.02.17',
+  },
+  {
+    missionId: 2,
+    userId: 'aaaaaaaaaaaa',
+    nickname: '안녕녕',
+    title: '부산 바캉스',
+    image:
+      'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyNDEyMTNfMjE2%2FMDAxNzM0MDQ5NDAxNzg0.Yk2qqTdXGk_UIdyc3ewKfXMXYJYxbqZgV5xfPIMillYg.X5lYnQ9t77bXgt1Ac3KK3pVF-6GxpC9cKars4bnwzaYg.JPEG%2FIMG_4214.JPG&type=sc960_832',
+    scrapNumber: 2,
+    applyNumber: 1,
+    applySuccessNumber: 1,
+    date: '2025.02.18',
+  },
+];
+
+function MissionList() {
+  return dummyData.map((mission) => (
+    <MissionCard
+      key={mission.id}
+      nickname={mission.nickname}
+      title={mission.title}
+      image={mission.image}
+      scrapNumber={mission.scrapNumber}
+      applySuccessNumber={mission.applySuccessNumber}
+      date={mission.date}
+    />
+  ));
+}
 
 function MissionPage() {
   return (
-    <>
-      <div className={style.missionHeader}>
-        <img src={LogoImg} className={style.LogoImg} />
-      </div>
-    </>
+    <div className={style.missionPageBg}>
+      <div className={style.noticeDiv}>📢 부산에서 열린 여러가지 미션에 참여해보세요!</div>
+      <MissionList />
+    </div>
   );
 }
 
